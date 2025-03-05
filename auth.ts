8aa433da36be0ef05fe357c7cd50a0ac4bd54aaa
lucasalbuquerque57 <75431427+lucasalbuquerque57@users.noreply.github.com>
2025-03-05 14:57:16 -0300

    indo fazer uma modificacao arriscada no authjs

--- auth.ts
+++ auth.ts
@@ -4,10 +4,32 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from '@/lib/db';
 import authConfig from '@authConfig';
 
+import {getUserById} from "@/util/user";
+
 export const { handlers: {GET, POST}, auth, signIn, signOut } = NextAuth({
     callbacks: {
+
+        async session({token, session}){
+            if (token.sub && session.user){
+                session.user.id =  token.sub;
+            }
+
+            if (token.role && session.user){
+                session.user.role = token.role;
+            }
+
+            return session;
+        },
+
         async jwt({token}){
-            console.log({token});
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+
+            if (!existingUser) return token;
+
+            token.role = existingUser.role;
+
             return token;
         }
     },
--- src/components/auth/header.tsx
+++ src/components/auth/header.tsx
@@ -8,13 +8,13 @@ const font = Poppins ({
 
 interface HeaderProps {
     label: string;
-};
+}
 
 export const Header = ({ label }: HeaderProps) => {
     return (
         <div className="w-full flex flex-col gap-4 items-center justify-center">
             <h1 className={cn('text-3xl font-semibold', font.className)}>
-                Auth
+                Autenticação
             </h1>
             <p className="text-muted-foreground text-sm">
                 {label}
