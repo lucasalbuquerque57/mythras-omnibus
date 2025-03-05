3d0d122349495c32abe6330ff3f9ef9d553e38ac
lucasalbuquerque57 <75431427+lucasalbuquerque57@users.noreply.github.com>
2025-03-05 00:00:21 -0300

    algumas verificações modificadas
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/actions/register.ts
    #	src/schemas/users/index.ts

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,39 +1,18 @@
 'use server';
 
-import * as bcrypt from 'bcryptjs';
 import * as z from 'zod';
-import {RegisterSchema} from '@/schemas/users';
-import {db} from '@/lib/db';
-import {getUserByEmail} from "@/util/user";
+import {RegisterSchema} from '@/schemas/users/index';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => { // ✅ Added `async`
     console.log(values);
     const validatedFields =  RegisterSchema.safeParse(values);
     if (!validatedFields.success){
         return {
-            error: 'Campos inválidos!',
-        };
+            error: 'Campos inválidos!'
+        }
     }
-    const {username, email, password} = validatedFields.data;
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const existingUser = await getUserByEmail(email);
-
-    if (existingUser) {
-        return {
-            error: 'E-mail já está sendo usado',
-        };
+    else {
+        return { success: 'E-mail enviado!' };
     }
 
-    await db.user.create({
-        data: {
-            username,
-            email,
-            password: hashedPassword,
-        },
-    });
-
-    // TODO: send a verification token e-mail
-
-    return { success: 'Usuário criado!' };
-};
+}
