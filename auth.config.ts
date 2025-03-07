8631f51f32f7ab639371fe5d4a70468a0b320b2e
lucasalbuquerque57 <75431427+lucasalbuquerque57@users.noreply.github.com>
2025-03-07 17:57:34 -0300

    fixed a bug that made passwords not be correctly validated

--- auth.config.ts
+++ auth.config.ts
@@ -18,20 +18,21 @@ export default { providers: [
         clientId: process.env.AUTH_DISCORD_ID,
         clientSecret: process.env.AUTH_DISCORD_SECRET,
     }),
-    Credentials({
-        async authorize(credentials){
-            const validatedFields = LoginSchema.safeParse(credentials);
-            if (validatedFields.success){
-                const { email, password} = validatedFields.data;
+        Credentials({
+            async authorize(credentials) {
+                const validatedFields = LoginSchema.safeParse(credentials);
+                if (!validatedFields.success) return null;
 
+                const { email, password } = validatedFields.data;
                 const user = await getUserByEmail(email);
                 if (!user || !user.password) return null;
 
                 const passwordsMatch = await bcrypt.compare(password, user.password);
-                if (!passwordsMatch) return user;
+                if (passwordsMatch) {
+                    return user;
+                }
 
+                return null;
             }
-            return null;
-        }
-    }),
+        }),
     ] } satisfies NextAuthConfig;
